7b46a4d606f4db05dde764d7e55c20dc5b6efa16
Claudio Saccella <cdsaccella@gmail.com>
2020-06-26 00:14:30 -0300

    changing index file

--- backend/router.js
+++ backend/router.js
@@ -5,7 +5,7 @@ import getMovies from './controllers/movies/get.js';
 const router = new Router();
 
 router.get('/', ({ response }) => {
-    response.body = 'Movies list rest api using deno runtime!';
+    response.body = 'Movies list rest api using deno runtime v2.0';
 });
 
 router
