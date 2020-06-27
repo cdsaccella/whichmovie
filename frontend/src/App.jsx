ea3086df8a44defaa100cd05dadbe6903a255de9
Claudio Saccella <cdsaccella@gmail.com>
2020-06-27 20:18:51 -0300

    setting up env variables

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>You are in {process.env.REACT_APP_ENV_NAME}</p>
         <p>Are you ready for this CABRONAZO?</p>
         <a
           className="nes-btn"
