90ae67497a9032caeed160ef103921b18b8e8203
Claudio Saccella <cdsaccella@gmail.com>
2020-06-27 22:02:32 -0300

    showing options

--- frontend/src/components/riddle/Riddle.jsx
+++ frontend/src/components/riddle/Riddle.jsx
@@ -1,27 +1,41 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import "./Riddle.css";
 
 function Riddle(props) {
-  const [riddle, setRiddle] = useState({});
+  const [image, setImage] = useState({});
+  const [options, setOptions] = useState([]);
+  const [riddle, setRiddle] = useState("");
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_URL}/riddles`)
       .then((res) => res.json())
       .then((res) => {
-        setRiddle(res.data);
+        setImage(res.data.image);
+        setOptions(res.data.options);
+        setRiddle(res.data.riddle);
         setIsLoading(false);
       })
       .catch((error) => console.log(error));
   }, []);
 
   return (
-    <div className="nes-container with-title">
+    <div className="nes-container with-title is-centered">
       <p className="title">Try it!</p>
       {isLoading && (
-        <progress class="nes-progress" value="90" max="100"></progress>
+        <progress className="nes-progress" value="90" max="100"></progress>
       )}
-      <img src={riddle.image}></img>
+      <img src={image}></img>
+      {options.map((option, index) => (
+        <div key={index} className="nes-field">
+          <div className="button-container">
+            <button type="button" className="nes-btn">
+              {option}
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
