7b64dc78d891cd03476891c926bcf78a9de6e2c0
Claudio Saccella <cdsaccella@gmail.com>
2020-07-09 02:29:03 -0300

    retorning image from function

--- functions/pixelator/pixelator.js
+++ functions/pixelator/pixelator.js
@@ -1,6 +1,32 @@
+var Jimp = require('jimp');
+
+var urlImage = 'https://picsum.photos/200/300';
+
 exports.handler = function (event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "Hello, World"
+  Jimp.default.read(urlImage)
+    .then(image => {
+      // Do stuff with the image.
+      image.resize(256, 256) // resize
+      // console.log('I read the i')
+      image.getBufferAsync('image/png').then(image => {
+        callback(null, {
+          statusCode: 200,
+          headers:
+          {
+            "Content-Type": "image/png"
+          },
+          body: image.toString('base64'),
+          isBase64Encoded: true
+        });
+      })
+        .catch(err => {
+          // Handle an exception.
+        });
+
+    })
+    .catch(err => {
+      // Handle an exception.
     });
+
+
 };
